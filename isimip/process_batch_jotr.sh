7eccc3a0d554c512862ed5412c3bae5b278c5813
lucialayr <lucia.sophie.layritz@posteo.de>
2026-02-19 02:16:53 +0000

    fixed script error, isimip data for tas and pr is now complete!

--- isimip/process_batch_jotr.sh
+++ isimip/process_batch_jotr.sh
@@ -8,9 +8,9 @@ lonmax=-115.445  # east
 latmin=33.655    # south
 latmax=34.345    # north
 
-for variable in pr; do
-for scenario in historical; do
-for model in  ukesm1-0-ll_r1i1p1f2 gfdl-esm4 ipsl-cm6a-lr_r1i1p1f1 mri-esm2-0_r1i1p1f1 mpi-esm1-2-hr_r1i1p1f1; do 
+for variable in tas; do
+for scenario in historical ssp126 ssp585; do
+for model in  ukesm1-0-ll_r1i1p1f2 gfdl-esm4_r1i1p1f1 ipsl-cm6a-lr_r1i1p1f1 mri-esm2-0_r1i1p1f1 mpi-esm1-2-hr_r1i1p1f1; do 
 	./process_unit.sh "${variable}" "${scenario}" "${model}" "${region}" "${lonmin}" "${lonmax}" "${latmin}" "${latmax}" &
 done
 done
--- isimip/process_batch_mojave.sh
+++ isimip/process_batch_mojave.sh
@@ -8,8 +8,8 @@ lonmax=-114.445  # east
 latmin=34.717    # south (increased to exclude southern row)
 latmax=36.091    # north
 
-for variable in pr; do
-for scenario in ssp126 ssp585; do
+for variable in tas; do
+for scenario in historical ssp126 ssp585; do
 for model in gfdl-esm4_r1i1p1f1 ipsl-cm6a-lr_r1i1p1f1 mri-esm2-0_r1i1p1f1 mpi-esm1-2-hr_r1i1p1f1 ukesm1-0-ll_r1i1p1f2; do 
 	./process_unit.sh "${variable}" "${scenario}" "${model}" "${region}" "${lonmin}" "${lonmax}" "${latmin}" "${latmax}" &
 done
